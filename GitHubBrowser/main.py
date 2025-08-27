adc41b29ad42ca15b6820dc5ae5c189aad54723d
randombuilder5129 <randombuilder5129@gmail.com>
2025-08-26 17:13:02 -0700

    Update main.py

--- GitHubBrowser/main.py
+++ GitHubBrowser/main.py
@@ -377,7 +377,5 @@ def home():
     return "✅ Service is running on Render!"
 
 if __name__ == "__main__":
-    # Render sets PORT automatically
     port = int(os.environ.get("PORT", 3000))
     app.run(host="0.0.0.0", port=port)
-
